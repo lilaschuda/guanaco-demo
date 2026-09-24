2dfa189c12b87dc8d4ffe16bf815940f7ab80d18
lilaschuda <lilaschuda@gmail.com>
2026-09-24 06:53:03 +0200

    adding features to pull from the dev board itself

---
+++ 05-guanaco-unoq/README.md
@@ -0,0 +1,4 @@
+# Example 05: Guanaco for Arduino Uno Q
+
+This module demonstrates the use of Guanaco as a nativa java onboard controller for the 
+Arduino Uno Q. Still under development.
---
+++ 05-guanaco-unoq/src/main/java/io/github/lilaschuda/guanaco/demo/unoq/route/ArduinoEvent.java
@@ -0,0 +1,22 @@
+package io.github.lilaschuda.guanaco.demo.unoq.route;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "@type")
+@JsonSubTypes({
+    @JsonSubTypes.Type(value = ArduinoEvent.PinChange.class, name = "PinChange"),
+    @JsonSubTypes.Type(value = ArduinoEvent.AnalogSample.class, name = "AnalogSample"),
+    @JsonSubTypes.Type(value = ArduinoEvent.Fault.class, name = "Fault")
+})
+public sealed interface ArduinoEvent permits 
+    ArduinoEvent.PinChange, 
+    ArduinoEvent.AnalogSample, 
+    ArduinoEvent.Fault {
+
+    record PinChange(int pin, boolean state, long timestampMs) implements ArduinoEvent {}
+    
+    record AnalogSample(int channel, int rawValue, double voltage) implements ArduinoEvent {}
+    
+    record Fault(int code, String message) implements ArduinoEvent {}
+}
